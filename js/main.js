4b8aadec0c5a9280cd167f97d52fa35b642411d3
Ejiro-Asiuwhu <45228014+Ejiro-Asiuwhu@users.noreply.github.com>
2019-12-05 08:53:54 +0100

    adding click event listener for translateX effect

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 function isEmail(email) {
-    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-    return regex.test(email);
+  return regex.test(email);
 }
 
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () =>
+  container.classList.add("right-panel-active")
+);
+
+signInButton.addEventListener("click", () =>
+  container.classList.remove("right-panel-active")
+);
